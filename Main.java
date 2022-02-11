4bcee0c4d5c9cba7904dd107fcc2798629757d27
MegaDen58 <96517069+MegaDen58@users.noreply.github.com>
2022-02-11 10:03:13 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        for (int x = 1; x <= 9; x++) {
+        for (int x = 1; x <= 10; x++) {
             for (int y = 1; y <= 10; y++) {
                 System.out.println(x + " x " + y + " = " + (x * y));
             }
